ed57496cd80f627612a73613d178f33459d9463d
Renuka Panigrahi <panigrahirenuka599@gmail.com>
2020-08-29 17:54:12 -0400

    Intail Project setup with SpringDataJpa with Redis Cache Managent as dependencies with gradle as build tool

---
+++ src/main/java/com/library/LibraryApplication.java
@@ -0,0 +1,15 @@
+package com.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching // to Enable Caching
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/library/api/BookService.java
@@ -0,0 +1,5 @@
+package com.library.api;
+
+public interface BookService {
+
+}
